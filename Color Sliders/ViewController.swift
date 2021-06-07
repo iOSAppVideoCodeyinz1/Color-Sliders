fd97493241929ac6cdc460a1956c58e85d080ed9
Theodore Yin <yinz1@rose-hulman.edu>
2021-06-07 17:05:27 -0400

    ibactions

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var alphaValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hello World")
+        
+        redSlider.value = 0.5
+        blueSlider.value = 0.0
+        greenSlider.value = 0.0
+        alphaSlider.value = 1.0
+        
+        
+        
     }
 
-
+    @IBAction func sliderChanged(_ sender: Any) {
+        
+        print("Slider Changed")
+        
+    }
+    
 }
 
