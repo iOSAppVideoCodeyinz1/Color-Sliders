c5de749cb0cd20c22f8a0d247c8e9b9ba6a228aa
Theodore Yin <yinz1@rose-hulman.edu>
2021-06-07 16:03:59 -0400

    Initial Commit

---
+++ Color Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color Sliders
+//
+//  Created by Theo Yin on 6/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
